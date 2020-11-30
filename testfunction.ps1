949f4853063d0510df63366fd2acdc0b9ec10e03
Morten <mhaavald@hotmail.com>
2020-11-29 16:58:29 -0800

    other user adding a line

--- testfunction.ps1
+++ testfunction.ps1
@@ -2,7 +2,10 @@
 {
     Write-Host "[testFunction] Start."
     
-       Write-Host "[testFunction] Number [$number]."
+    Write-Host "[testFunction] Number [$number]."
+
+    Write-Host "[testFunction] Other user adding this line."
+
 
     Write-Host "[testFunction] End."
 }
