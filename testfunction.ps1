fcf86234a0f16b8cd57899296bc7b5a6f854e4ed
Morten <mhaavald@hotmail.com>
2020-11-29 16:47:23 -0800

    add param

--- testfunction.ps1
+++ testfunction.ps1
@@ -1,7 +1,8 @@
-﻿Function testfunction
+﻿Function testfunction($number)
 {
     Write-Host "[testFunction] Start."
     
+       Write-Host "[testFunction] Number [$number]."
 
     Write-Host "[testFunction] End."
 }
